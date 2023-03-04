52aadf780fdb837adf2b7f225327b60ff3a6cc48
Raj Gohil <raj.famous007@gmail.com>
2023-03-04 16:31:45 +0530

    Implementation flipping an image of program.

---
+++ src/com/raj/FlippingAnImage.java
@@ -0,0 +1,65 @@
+/*
+Link: https://leetcode.com/problems/flipping-an-image/
+
+Flipping an Image
+Given an n x n binary matrix image, flip the image horizontally, then invert it, and return the resulting image.
+
+To flip an image horizontally means that each row of the image is reversed.
+
+For example, flipping [1,1,0] horizontally results in [0,1,1].
+To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0.
+For example, inverting [0,1,1] results in [1,0,0].
+
+Example 1:
+Input: image = [[1,1,0],[1,0,1],[0,0,0]]
+Output: [[1,0,0],[0,1,0],[1,1,1]]
+Explanation: First reverse each row: [[0,1,1],[1,0,1],[0,0,0]].
+Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+
+Example 2:
+Input: image = [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+Output: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+Explanation: First reverse each row: [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+
+Constraints:
+
+n == image.length
+n == image[i].length
+1 <= n <= 20
+images[i][j] is either 0 or 1.
+ */
+package com.raj;
+
+import java.util.Arrays;
+
+public class FlippingAnImage {
+    public static void main(String[] args) {
+        int[][] image = new int[][]{
+                {1, 1, 0, 0},
+                {1, 0, 0, 1},
+                {0, 1, 1, 1},
+                {1, 0, 1, 0}
+        };
+
+        int length = image.length;
+
+        for (int i = 0; i < length; i++) {
+            int start = 0;
+            int end = length - 1;
+            int[] currentArr = image[i];
+
+            while (start < end) {
+                int t = currentArr[start] == 0 ? 1 : 0;
+                currentArr[start] = currentArr[end] == 0 ? 1 : 0;
+                currentArr[end] = t;
+                start++;
+                end--;
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            System.out.println(Arrays.toString(image[i]));
+        }
+    }
+}
