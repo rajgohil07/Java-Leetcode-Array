50f16ad5cc8290c6c90f96f3ac27bc9eb282b456
Raj Gohil <raj.famous007@gmail.com>
2023-03-05 17:37:10 +0530

    Implemented the program for the ransom note.

---
+++ src/com/raj/RansomNote.java
@@ -0,0 +1,54 @@
+/*
+Ransom Note
+Link: https://leetcode.com/problems/ransom-note/
+Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+
+Each letter in magazine can only be used once in ransomNote.
+
+Example 1:
+Input: ransomNote = "a", magazine = "b"
+Output: false
+
+Example 2:
+Input: ransomNote = "aa", magazine = "ab"
+Output: false
+
+Example 3:
+Input: ransomNote = "aa", magazine = "aab"
+Output: true
+
+Constraints:
+1 <= ransomNote.length, magazine.length <= 105
+ransomNote and magazine consist of lowercase English letters.
+*/
+package com.raj;
+
+public class RansomNote {
+    public static void main(String[] args) {
+        String ransomNote = "aa";
+        String magazine = "aab";
+        char[] ransomNoteArr = ransomNote.toCharArray();
+        char[] magazineArr = magazine.toCharArray();
+        int rl = ransomNoteArr.length;
+        int ml = magazineArr.length;
+
+        if (rl > ml) {
+            System.out.println("String not matched");
+        }
+
+        for (int i = 0; i < rl; i++) {
+            boolean is_matched = false;
+            for (int j = 0; j < ml; j++) {
+                if (ransomNoteArr[i] == magazineArr[j]) {
+                    is_matched = true;
+                    magazineArr[j] = 'A';
+                    break;
+                }
+            }
+            if (!is_matched) {
+                System.out.println("String not matched");
+            }
+        }
+        System.out.println("String matched!");
+    }
+}
