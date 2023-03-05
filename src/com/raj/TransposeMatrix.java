03c8ff210e41aa4a970fbcb19bd9c8fb3362e7ca
Raj Gohil <raj.famous007@gmail.com>
2023-03-05 12:23:09 +0530

    Have added the program for the transpose matrix.

---
+++ src/com/raj/TransposeMatrix.java
@@ -0,0 +1,52 @@
+/*
+Transpose Matrix
+Link: https://leetcode.com/problems/transpose-matrix/
+
+Given a 2D integer array matrix, return the transpose of matrix.
+
+The transpose of a matrix is the matrix flipped over its main diagonal, switching the matrix's row and column indices.
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[1,4,7],[2,5,8],[3,6,9]]
+
+Example 2:
+Input: matrix = [[1,2,3],[4,5,6]]
+Output: [[1,4],[2,5],[3,6]]
+
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 1000
+1 <= m * n <= 105
+-109 <= matrix[i][j] <= 109
+ */
+package com.raj;
+
+import java.util.Arrays;
+
+public class TransposeMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{
+//                {1},
+//                {2},
+//                {3}
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+
+        int pi = matrix.length;
+        int ci = matrix[0].length;
+        int[][] ans = new int[ci][pi];
+
+        for (int i = 0; i < pi; i++) {
+            for (int j = 0; j < ci; j++) {
+                ans[j][i] = matrix[i][j];
+            }
+        }
+
+        for (int[] arr : ans) {
+            System.out.println("arr = " + Arrays.toString(arr));
+        }
+    }
+}
