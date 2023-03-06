a09f1fd66d0a7c5b1461fe4382a6622a2b149f5c
Raj Gohil <raj.famous007@gmail.com>
2023-03-07 00:15:29 +0530

    Function to add n unique integers sum up to zero.

---
+++ src/com/raj/FindNUniqueIntegersSumUpToZero.java
@@ -0,0 +1,39 @@
+/*
+Find N Unique Integers Sum up to Zero
+Link: https://leetcode.com/problems/find-n-unique-integers-sum-up-to-zero/
+
+Given an integer n, return any array containing n unique integers such that they add up to 0.
+
+Example 1:
+Input: n = 5
+Output: [-7,-1,1,3,4]
+Explanation: These arrays also are accepted [-5,-1,1,2,3] , [-3,-1,2,-2,4].
+
+Example 2:
+Input: n = 3
+Output: [-1,0,1]
+
+Example 3:
+Input: n = 1
+Output: [0]
+
+Constraints:
+1 <= n <= 1000
+ */
+package com.raj;
+
+import java.util.Arrays;
+
+public class FindNUniqueIntegersSumUpToZero {
+    public static void main(String[] args) {
+        int n = 5;
+        int c = n / 2;
+        int[] ans = new int[n];
+        for (int i = 0; i < c; i++) {
+            ans[i] = -1;
+            ans[c + i] = 1;
+        }
+        System.out.println("ans = " + Arrays.toString(ans));
+    }
+
+}
