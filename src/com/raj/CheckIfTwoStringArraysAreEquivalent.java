3d6a48914d2f62f8b5ff2b84c36fd262c51d05fc
Raj Gohil <raj.famous007@gmail.com>
2023-03-10 11:32:19 +0530

    Use of string builder in the program to Check If Two String Arrays are Equivalent.

--- src/com/raj/CheckIfTwoStringArraysAreEquivalent.java
+++ src/com/raj/CheckIfTwoStringArraysAreEquivalent.java
@@ -5,18 +5,18 @@ public class CheckIfTwoStringArraysAreEquivalent {
         // Initialization.
         String[] word1 = new String[]{"a", "cb"};
         String[] word2 = new String[]{"ab", "c"};
-        String wordStr1 = "";
-        String wordStr2 = "";
+        StringBuilder wordStr1 = new StringBuilder();
+        StringBuilder wordStr2 = new StringBuilder();
 
         // Logic.
         for (String w : word1) {
-            wordStr1 += w;
+            wordStr1.append(w);
         }
         for (String w : word2) {
-            wordStr2 += w;
+            wordStr2.append(w);
         }
 
         // Display the result.
-        System.out.println("Is string same: " + wordStr1.equals(wordStr2));
+        System.out.println("Is string same: " + wordStr1.toString().equals(wordStr2.toString()));
     }
 }
