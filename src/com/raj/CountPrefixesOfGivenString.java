f1a681c66afa5cc69dbc9606414ddd0155de1598
Raj Gohil <raj.famous007@gmail.com>
2023-03-13 16:09:42 +0530

    Have implemented program to count prefixes of a given string.

---
+++ src/com/raj/CountPrefixesOfGivenString.java
@@ -0,0 +1,51 @@
+/*
+Count Prefixes of a Given String
+Link: https://leetcode.com/problems/count-prefixes-of-a-given-string/
+
+You are given a string array words and a string s, where words[i] and s comprise only of lowercase English letters.
+
+Return the number of strings in words that are a prefix of s.
+
+A prefix of a string is a substring that occurs at the beginning of the string. A substring is a contiguous sequence of characters within a string.
+
+Example 1:
+Input: words = ["a","b","c","ab","bc","abc"], s = "abc"
+Output: 3
+Explanation:
+The strings in words which are a prefix of s = "abc" are:
+"a", "ab", and "abc".
+Thus the number of strings in words which are a prefix of s is 3.
+
+Example 2:
+Input: words = ["a","a"], s = "aa"
+Output: 2
+Explanation:
+Both of the strings are a prefix of s.
+Note that the same string can occur multiple times in words, and it should be counted each time.
+
+Constraints:
+
+1 <= words.length <= 1000
+1 <= words[i].length, s.length <= 10
+words[i] and s consist of lowercase English letters only.
+ */
+package com.raj;
+
+public class CountPrefixesOfGivenString {
+    public static void main(String[] args) {
+        // Initialization.
+        String[] words = new String[]{"a", "b", "c", "ab", "bc", "abc"};
+        String s = "abc";
+        int count = 0;
+
+        // Logic.
+        for (String w : words) {
+            if (s.startsWith(w)) {
+                count++;
+            }
+        }
+
+        // Display the result.
+        System.out.println("count: " + count);
+    }
+}
