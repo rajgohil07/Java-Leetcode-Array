08460203083f1c23f3626d8194a517eb51ec3c5b
Raj Gohil <raj.famous007@gmail.com>
2023-04-19 00:32:36 +0530

    Have implemented the program to find the merge strings.

---
+++ src/com/raj/MergeStringsAlternately.java
@@ -0,0 +1,69 @@
+/*
+Merge Strings Alternately
+Link: https://leetcode.com/problems/merge-strings-alternately/
+
+You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+Return the merged string.
+
+Example 1:
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
+
+Example 2:
+Input: word1 = "ab", word2 = "pqrs"
+Output: "apbqrs"
+Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+word1:  a   b
+word2:    p   q   r   s
+merged: a p b q   r   s
+
+Example 3:
+Input: word1 = "abcd", word2 = "pq"
+Output: "apbqcd"
+Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+word1:  a   b   c   d
+word2:    p   q
+merged: a p b q c   d
+
+Constraints:
+
+1 <= word1.length, word2.length <= 100
+word1 and word2 consist of lowercase English letters.
+ */
+package com.raj;
+
+public class MergeStringsAlternately {
+    public static void main(String[] args) {
+        // Initialization.
+        String word1 = "abc";
+        String word2 = "pqr";
+        StringBuilder ans = new StringBuilder();
+        int loopLength = word1.length();
+        int word1Length = word1.length();
+        int word2Length = word2.length();
+
+        // Logic.
+        if (word1Length > word2Length) {
+            loopLength = word2Length;
+        }
+
+        for (int i = 0; i < loopLength; i++) {
+            ans.append(word1.charAt(i));
+            ans.append(word2.charAt(i));
+        }
+
+        if (word1Length > word2Length) {
+            ans.append(word1.substring(word2Length));
+        } else if (word1Length < word2Length) {
+            ans.append(word2.substring(word1Length));
+        }
+
+        // Display the result.
+        System.out.println("Finally the merge string is: " + ans.toString());
+    }
+}
