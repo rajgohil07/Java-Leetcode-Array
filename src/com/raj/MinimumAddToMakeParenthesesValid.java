c665a580eea0dc5a7d05d41bf15e2c1a5cd3362b
Raj Gohil <raj.famous007@gmail.com>
2023-04-26 13:38:20 +0530

    Have implemented the program to add the parentheses to make the valid parentheses.

---
+++ src/com/raj/MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,48 @@
+/*
+Minimum Add to Make Parentheses Valid
+Link: https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+
+A parentheses string is valid if and only if:
+
+It is the empty string,
+It can be written as AB (A concatenated with B), where A and B are valid strings, or
+It can be written as (A), where A is a valid string.
+You are given a parentheses string s. In one move, you can insert a parenthesis at any position of the string.
+
+For example, if s = "()))", you can insert an opening parenthesis to be "(()))" or a closing parenthesis to be "())))".
+Return the minimum number of moves required to make s valid.
+
+Example 1:
+Input: s = "())"
+Output: 1
+
+Example 2:
+Input: s = "((("
+Output: 3
+
+Constraints:
+1 <= s.length <= 1000
+s[i] is either '(' or ')'.
+ */
+package com.raj;
+
+public class MinimumAddToMakeParenthesesValid {
+    public static void main(String[] args) {
+        // Initialization.
+        String s = "())";
+        int opening = 0;
+        int closing = 0;
+
+        // Logic.
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                opening++;
+            } else {
+                closing++;
+            }
+        }
+
+        // Display the result.
+        System.out.println("The parentheses required to make the valid parentheses is: " + Math.abs(opening - closing));
+    }
+}
