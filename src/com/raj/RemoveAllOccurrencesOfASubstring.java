c2f6be7ad8d10630bfb40866503485c4835c209f
Raj Gohil <raj.famous007@gmail.com>
2023-05-01 12:35:52 +0530

    Have implemented the program to find the final string after removing all the occurrences of a substring:.

---
+++ src/com/raj/RemoveAllOccurrencesOfASubstring.java
@@ -0,0 +1,31 @@
+/*
+Remove All Occurrences of a Substring
+Link: https://leetcode.com/problems/remove-all-occurrences-of-a-substring/
+ */
+package com.raj;
+
+public class RemoveAllOccurrencesOfASubstring {
+    public static void main(String[] args) {
+        // Initialization.
+        String s = "daabcbaabcbc";
+        String part = "abc";
+        StringBuilder ans = new StringBuilder(s);
+
+        // Logic.
+        recursiveStringReplace(ans, part);
+
+        // Display the result.
+        System.out.println("The final string after removing all the occurrences of a substring: " + ans+".");
+    }
+
+    // Recursive call the function itself to replace the string with the part.
+    private static StringBuilder recursiveStringReplace(StringBuilder s, String part) {
+        for (int i = 0; i <= s.length() - part.length(); i++) {
+            if (s.substring(i, i + part.length()).equals(part)) {
+                s = s.delete(i, i + part.length());
+                recursiveStringReplace(s, part);
+            }
+        }
+        return s;
+    }
+}
